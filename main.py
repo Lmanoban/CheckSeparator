204b684b0eb1cf9a94e0308d710a02d557c6b6e3
Mihail Podoshevskiy <letsplayonut@gmail.com>
2021-09-19 18:12:23 +0300

    Added BoxLayout

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ from kivymd.icon_definitions import md_icons
 from kivymd.font_definitions import fonts
 
 KV = '''
+# https://stackoverflow.com/questions/65698145/kivymd-tab-name-containing-icons-and-text
+# this import will prevent disappear tabs through some clicks on them)))
+#:import md_icons kivymd.icon_definitions.md_icons
+#:import fonts kivymd.font_definitions.fonts
+
 # Menu item in the DrawerList list.
 <ItemDrawer>:
     theme_text_color: "Custom"
@@ -41,13 +46,13 @@ KV = '''
             source: "data/logo/kivy-icon-256.png"
 
     MDLabel:
-        text: "KivyMD library"
+        text: app.title
         font_style: "Button"
         size_hint_y: None
         height: self.texture_size[1]
 
     MDLabel:
-        text: "kivydevelopment@gmail.com"
+        text: app.by_who
         font_style: "Caption"
         size_hint_y: None
         height: self.texture_size[1]
@@ -71,13 +76,90 @@ Screen:
                     orientation: 'vertical'
 
                     MDToolbar:
-                        title: "Check Separator"
+                        title: app.title
                         elevation: 10
                         left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                        right_action_items: [["star-outline", lambda x: app.on_star_click()]]
+                        md_bg_color: 0, 0, 0, 1
 
                     MDTabs:
                         id: tabs
+                        on_tab_switch: app.on_tab_switch(*args)
+                        height: "48dp"
+                        tab_indicator_anim: False
+                        background_color: 0.1, 0.1, 0.1, 1
+                        
+                        Tab:
+                            id: tab1
+                            name: 'tab1'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['calculator-variant']}[/size][/font] Input"
+                            
+                            BoxLayout:
+                                orientation: 'vertical'
+                                padding: "10dp"
 
+                                BoxLayout:
+                                    orientation: 'horizontal'
+
+                                    MDIconButton:
+                                        icon: "calendar-month"
+
+                                    MDTextField:
+                                        id: start_date
+
+                                BoxLayout:
+                                    orientation: 'horizontal'
+ 
+                                    MDIconButton:
+                                        icon: "cash"
+
+                                    MDTextField:
+                                        id: loan
+                                        name: 'loan'
+
+                                BoxLayout:
+                                    orientation: 'horizontal'
+
+                                    MDIconButton:
+                                        icon: "clock-time-five-outline"
+
+                                    MDTextField:
+                                        hint_text: "Month"
+
+
+                                BoxLayout:
+                                    orientation: 'horizontal'
+
+                                    MDIconButton:
+                                        icon: "bank"
+
+                                    MDTextField:
+                                        hint_text: "Interest, %"
+
+                                    MDTextField:
+                                        id: payment_type
+                                        hint_text: "Payment type"
+                                        on_focus: if self.focus: app.menu.open()
+
+                        Tab:
+                            id: tab2
+                            name: 'tab2'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['table-large']}[/size][/font] Table"
+                            
+                        Tab:
+                            id: tab3
+                            name: 'tab3'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['chart-areaspline']}[/size][/font] Graph"
+                            
+                        Tab:
+                            id: tab4
+                            name: 'tab4'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['chart-pie']}[/size][/font] Chart"
+                            
+                        Tab:
+                            id: tab5
+                            name: 'tab5'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['book-open-variant']}[/size][/font] Sum"
 
         MDNavigationDrawer:
             id: nav_drawer
@@ -85,10 +167,6 @@ Screen:
             ContentNavigationDrawer:
                 id: content_drawer
 '''
-
-
-class Tab(MDFloatLayout, MDTabsBase):
-    pass
 
 
 class ContentNavigationDrawer(BoxLayout):
@@ -112,30 +190,59 @@ class DrawerList(ThemableBehavior, MDList):
         instance_item.text_color = self.theme_cls.primary_color
 
 
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+
+
 class CheckSeparatorApp(MDApp):
+    title = "Check Separator"
+    by_who = "by Mihail Podoshevskiy"
+
     def build(self):
         return Builder.load_string(KV)
 
     def on_start(self):
-        icons_item = {
-            "folder": "My files",
-            "account-multiple": "Shared with me",
-            "star": "Starred",
-            "history": "Recent",
-            "checkbox-marked": "Shared with me",
-            "upload": "Upload",
+        icons_item_menu_lines = {
+            "account-cowboy-hat": "About author",
+            "youtube": "My YouTube",
+            "coffee": "Donate author",
+            "github": "Source code",
+            "share-variant": "Share app",  #air-horn
+            "shield-sun": "Dark/Light",
         }
-        for icon_name in icons_item.keys():
+        icons_item_menu_tabs = {
+            "calculator-variant": "Input",  #ab-testing
+            "table-large": "Table",
+            "chart-areaspline": "Graph",
+            "chart-pie": "Chart",  # chart-arc
+            "book-open-variant": "Sum",
+        }
+        for icon_name in icons_item_menu_lines.keys():
             self.root.ids.content_drawer.ids.md_list.add_widget(
-                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+                ItemDrawer(icon=icon_name,
+                           text=icons_item_menu_lines[icon_name])
             )
-        # for name_tab in list(md_icons.keys())[15:30]:
-        #     self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
+        # To auto generate tabs
+        # for icon_name, name_tab in icons_item_menu_tabs.items():
+        #     self.root.ids.tabs.add_widget(
+        #         Tab(
+        #             text=f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/size][/font] {name_tab}"
+        #         )
+        #     )
 
-        for icon_name, name_tab in icons_item.items():
-            self.root.ids.tabs.add_widget(
-                Tab(text=f"[ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/font][/ref] {name_tab}")
-            )
+    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        print("tab clicked!"+tab_text)
+
+    def on_star_click(self):
+        pass
 
 
 CheckSeparatorApp().run()
