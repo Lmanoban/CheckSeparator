f654a790b6a0a63867e624a4aba103f97fc09096
Mihail Podoshevskiy <letsplayonut@gmail.com>
2021-09-21 16:43:25 +0300

    Finally but without tests

--- main.py
+++ main.py
@@ -1,313 +1,17 @@
-# from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ListProperty
-
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import OneLineIconListItem, MDList
-
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.floatlayout import MDFloatLayout
-
-# from kivymd.font_definitions import fonts
-# from kivymd.icon_definitions import md_icons
-
-# from kivymd.uix.menu import MDDropdownMenu
-# from kivy.clock import Clock
-
 from kivymd.uix.picker import MDDatePicker
 import datetime
-# import calendar
 import os
 import webbrowser
 
-KV = '''
-# https://stackoverflow.com/questions/65698145/kivymd-tab-name-containing-icons-and-text
-# this import will prevent disappear tabs through some clicks on them)))
-#:import md_icons kivymd.icon_definitions.md_icons
-#:import fonts kivymd.font_definitions.fonts
-
-# Menu item in the DrawerList list.
-<ItemDrawer>:
-    theme_text_color: "Custom"
-    on_release: self.parent.set_color_item(self)
-
-    IconLeftWidget:
-        id: icon
-        icon: root.icon
-        theme_text_color: "Custom"
-        text_color: root.text_color
-
-
-<ContentNavigationDrawer>:
-    orientation: "vertical"
-    padding: "8dp"
-    spacing: "8dp"
-    
-
-    AnchorLayout:
-        anchor_x: "center"
-        size_hint_y: None
-        height: avatar.height
-
-        Image:
-            id: avatar
-            size_hint: None, None
-            size: "56dp", "56dp"
-            source: "data/logo/526.png"
-
-    MDLabel:
-        halign: "center"
-        text: app.title
-        font_style: "Button"
-        size_hint_y: None
-        height: self.texture_size[1]
-
-    MDLabel:
-        halign: "center"
-        text: app.by_who
-        font_style: "Caption"
-        size_hint_y: None
-        height: self.texture_size[1]
-        
-    AnchorLayout:
-        anchor_x: "center"
-        size_hint_y: None
-        height: avatar.height    
-        
-        MDIconButton:
-            icon: "account-cowboy-hat"
-            user_font_size: "30sp"
-            on_release: app.author_link()
-        
-    MDLabel:
-        halign: "center"
-        orientation: 'horizontal'
-        text: "About Author"
-        font_style: "Caption"
-        size_hint_y: None
-        height: self.texture_size[1]
-        
-    AnchorLayout:
-        anchor_x: "center"
-        size_hint_y: None
-        height: avatar.height    
-        
-        MDIconButton:
-            icon: "github"
-            user_font_size: "30sp"
-            on_release: app.code_link()
-        
-    MDLabel:
-        halign: "center"
-        orientation: 'horizontal'
-        text: "Source Code"
-        font_style: "Caption"
-        size_hint_y: None
-        height: self.texture_size[1]
-        
-    
-        
-    ScrollView:
-
-        DrawerList:
-            id: md_list
-
-
-
-Screen:
-
-    MDNavigationLayout:
-
-        ScreenManager:
-
-            Screen:
-                           
-                BoxLayout:
-                    orientation: 'vertical'
-
-                    MDToolbar:
-                        title: app.title
-                        elevation: 10
-                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
-                        md_bg_color: 0, 0, 0, 1
-
-                    MDTabs:
-                        id: tabs
-                        height: "48dp"
-                        tab_indicator_anim: False
-                        background_color: 0.1, 0.1, 0.1, 1
-                        
-                        Tab:
-                            id: tab1
-                            name: 'tab1'
-                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['calculator-variant']}[/size][/font] Check Separator"
-                            
-                            BoxLayout:
-                                orientation: 'vertical'
-                                padding: "10dp"
-
-                                BoxLayout:
-                                    orientation: 'horizontal'
-
-                                    MDIconButton:
-                                        icon: "calendar-month"
-
-                                    MDTextField:
-                                        id: start_date
-                                        on_focus: if self.focus: app.date_dialog.open()
-                                
-                                BoxLayout:
-                                    orientation: 'horizontal'
-
-                                    MDIconButton:
-                                        icon: "cash"
-
-                                    MDTextField:
-                                        hint_text: "Enter total score"
-                                        id: total_score
-                                        name: "total score"
-                                        
-                                BoxLayout:
-                                    orientation: 'horizontal'
-
-                                    MDIconButton:
-                                        icon: "account-supervisor"
-
-                                    MDTextField:
-                                        hint_text: "Enter numerous of people(only positive value)"
-                                        id: numerous_of_people
-                                        name: "numerous of people"
-                                        
-                                BoxLayout:
-                                    orientation: 'horizontal'
-
-                                    MDIconButton:
-                                        icon: "account" 
-
-                                    MDTextField:
-                                        hint_text: "Enter person name"
-                                        id: name
-                                        name: 'name'
-
-                                BoxLayout:
-                                    orientation: 'horizontal'
-
-                                    MDIconButton:
-                                        icon: "calculator-variant" 
-
-                                    MDTextField:
-                                        hint_text: "Enter cost of meal then push button"
-                                        id: cash
-                                        name: 'cash'
-                                        
-                                    MDIconButton:
-                                        icon: "gesture-tap-button"
-                                        on_release: app.calc_table(*args)
-                                        
-                                    MDIconButton:
-                                        icon: "stop-circle"
-                                        on_release: app.calc_table_close(*args)
-                                        
-                                BoxLayout:
-                                    orientation: 'horizontal'
-                                    
-                                    MDRectangleFlatIconButton:
-                                        icon: "clipboard-text-off"
-                                        text: "Clear Result List"
-                                        theme_text_color: "Custom"
-                                        text_color: 0.1, 0.1, 0.1, 1.0
-                                        line_color: 0.1, 0.1, 0.1, 1.0
-                                        icon_color: 0.1, 0.1, 0.1, 1.0
-                                        on_release: app.clear_result_list(*args)
-
-                        Tab:
-                            id: tab2
-                            name: 'tab2'
-                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['table-large']}[/size][/font] Result List"
-                            
-                            BoxLayout:
-                                orientation: "vertical"
-                                padding: "10dp"
-                                                             
-                                BoxLayout:
-                                    size_hint_y: .1
-                                    size_hint_x: 1
-                                    orientation: "vertical"
-                                    padding: "10dp"
-                                        
-                                    MDLabel:
-                                        halign: "center"
-                                        text: "Result List"
-                                                                       
-                                BoxLayout:
-                                    size_hint_y: .1
-                                    size_hint_x: 1
-                                    orientation: "vertical"
-                                    padding: "10dp"
-
-                                    MDIcon:
-                                        halign: "center"
-                                        icon: "clipboard-list"
-                                
-                                BoxLayout:
-                                    orientation: "vertical"
-                                    padding: "10dp"
-
-                                    ScrollView:
-
-                                        MDList:
-                                            id: table_list
-                                            
-                                BoxLayout:  
-                                    orientation: 'horizontal'
-                                    
-                                    MDRectangleFlatIconButton:
-                                        pos_hint: {"center_x": .5}
-                                        icon: "cached"
-                                        text: "Read cache log"
-                                        theme_text_color: "Custom"
-                                        text_color: 0.1, 0.1, 0.1, 1.0
-                                        line_color: 0.1, 0.1, 0.1, 1.0
-                                        icon_color: 0.1, 0.1, 0.1, 1.0
-                                        on_release: app.read_cache_doc()
-                                        
-        MDNavigationDrawer:
-            id: nav_drawer
-            
-            ContentNavigationDrawer:
-                id: content_drawer
-                
-<ItemTable>:
-    size_hint_y: None
-    height: "42dp"
-
-    canvas:
-        Color:
-            rgba: root.color
-        Rectangle:
-            size: self.size
-            pos: self.pos
-
-    MDLabel:
-        text: root.num
-        halign: "center"
-    MDLabel:
-        text: root.name
-        halign: "center"
-    MDLabel:
-        text: root.date
-        halign: "center"
-    MDLabel:
-        text: root.res_sum
-        halign: "center"
-    MDLabel:
-        text: root.percent
-        halign: "center"
-                        
-'''
-
+KV = open('app_interface.kv', 'r').read()
 result_sum = 0
 peoples = 1
 sum_of_check = 0
@@ -431,8 +135,6 @@ class CheckSeparatorApp(MDApp):
             start_sum = float(start_sum)
             result_sum += start_sum
             sum_of_check += start_sum
-            print(result_sum)
-            print(sum_of_check)
         except ValueError:
             self.screen.ids.cash.text = "The price cannot be literal"
 
@@ -446,7 +148,6 @@ class CheckSeparatorApp(MDApp):
         global result_sum
         global peoples
         global sum_of_check
-        print(result_sum)
         try:
             numerous_of_people = self.screen.ids.numerous_of_people.text
             numerous_of_people = int(numerous_of_people)
@@ -469,7 +170,6 @@ class CheckSeparatorApp(MDApp):
                                 )
                             )
                             self.save_in_cache()
-                            print(peoples)
                             peoples += 1
                             result_sum = 0
                         else:
@@ -485,7 +185,6 @@ class CheckSeparatorApp(MDApp):
                                 )
                             )
                             self.save_in_cache()
-                            print(peoples)
                             peoples += 1
                             result_sum = 0
                 else:
@@ -505,7 +204,6 @@ class CheckSeparatorApp(MDApp):
                                 )
                             )
                             self.save_in_cache_zero()
-                            print(peoples)
                             peoples += 1
                             result_sum = 0
                         else:
@@ -519,7 +217,6 @@ class CheckSeparatorApp(MDApp):
                                 )
                             )
                             self.save_in_cache_zero()
-                            print(peoples)
                             peoples += 1
                             result_sum = 0
                 else:
